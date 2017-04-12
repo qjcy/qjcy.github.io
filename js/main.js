d820a783b3a47329556ab9ff0a8394810c3625d1
qjcy <573413948@qq.com>
2017-04-12 14:04:56 +0800

    给页面增加js功能，新添一个文章页面和文章页面的css文件

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
-/**
- * Created by 李全杰 on 2017/4/11.
- */
+// 自动加载函数
+;$(function () {
+    'use strict';
+    var sidebar = $('#sidebar');  //选择侧栏
+    var mask = $('.mask');  //选择蒙版
+    var sidebar_trigger = $('#sidebar_trigger'); //选择侧栏触发器
+    var backBtn = $('.back-to-top');    //选择返回顶部按钮
+    var navbar = $('#navbar');
+
+    navbar.delegate('li','click',function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+    sidebar_trigger.on('click',showSidebar);    //监听侧栏触发器点击事件
+    mask.on('click',hideSidebar);   //监听蒙版点击事件
+    backBtn.on('click',toTop);  //监听返回顶部按钮点击事件
+    $(window).on('scroll',showBackBtn); //监听window的scroll事件
+    //页面一刷新就触发scroll事件
+    // $(window).trigger('scroll');
+
+    //显示侧栏
+    function showSidebar() {
+        mask.fadeIn();  //显示蒙版
+        sidebar.animate({   //调整侧栏相关的css
+            'right': 0
+        })
+    }
+
+    //隐藏侧栏
+    function hideSidebar() {
+        mask.fadeOut(); //隐藏蒙版
+        sidebar.animate({   //调整侧栏相关的css
+            'right': -sidebar.width()+'px'
+        })
+    }
+
+    function toTop() {
+        $('html,body').animate({
+            scrollTop: 0
+        },800);
+    }
+
+    function showBackBtn() {
+        //如果已滚动的部分高于窗口的高度
+        if($(window).scrollTop()>$(window).height()){
+            backBtn.fadeIn();
+        } else{
+            backBtn.fadeOut();
+        }
+    }
+});
